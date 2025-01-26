d8374852427754ef7d37f675587573f01b707c8d
EMC641 <108538423+EMC641@users.noreply.github.com>
2025-01-25 17:20:52 -0800

    encoder work

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/encoder_test.java
@@ -0,0 +1,43 @@
+
+package org.firstinspires.ftc.teamcode.autonomous;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+@TeleOp(name = "Encoder Test ")
+public class encoder_test extends OpMode {
+    DcMotor motor;
+    double ticks = 1425.1;
+    double newTarget;
+    @Override
+    public void init() {
+        motor = hardwareMap.get(DcMotor.class, "motor");
+        telemetry.addData("Hardware: ", "Initialized");
+        motor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+    }
+
+    @Override
+    public void loop() {
+        if(gamepad1.a){
+            encoder(2);
+        }
+        telemetry.addData("Motor Ticks: ", motor.getCurrentPosition());
+        if(gamepad1.b){
+            tracker();
+        }
+
+    }
+    public void encoder(int turnage){
+        newTarget = ticks/turnage;
+        motor.setTargetPosition((int)newTarget);
+        motor.setPower(0.3);
+        motor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+    }
+    public void tracker(){
+        motor.setTargetPosition(0);
+        motor.setPower(0.8);
+        motor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+    }
+
+}
